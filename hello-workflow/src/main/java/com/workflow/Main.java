15f3e1e1b042ecf7178910a449f5387ea85fded2
Prashant Aghara <prashant.aghara2111@gmail.com>
2025-11-23 12:38:29 +0530

    Run Workflow Application

--- hello-workflow/src/main/java/com/workflow/Main.java
+++ hello-workflow/src/main/java/com/workflow/Main.java
@@ -1,6 +1,8 @@
 package com.workflow;
 
 import io.temporal.client.WorkflowClient;
+import io.temporal.client.WorkflowOptions;
+import io.temporal.client.WorkflowStub;
 import io.temporal.serviceclient.WorkflowServiceStubs;
 import io.temporal.worker.Worker;
 import io.temporal.worker.WorkerFactory;
@@ -14,5 +16,17 @@ public class Main {
         Worker worker = factory.newWorker("greeting-tasks");
         worker.registerWorkflowImplementationTypes(GreetImpl.class);
         factory.start();
+
+        // Run from Application itself
+//        WorkflowOptions options = WorkflowOptions.newBuilder()
+//                .setWorkflowId("application-run")
+//                .setTaskQueue("greeting-tasks")
+//                .build();
+//
+//        Greeting workflow = client.newWorkflowStub(Greeting.class, options);
+//        String greeting = workflow.greet("Prashant");
+//        String workflowId = WorkflowStub.fromTyped(workflow).getExecution().getWorkflowId();
+//        System.out.println("Workflow Id : " + workflowId);
+//        System.out.println(greeting);
     }
 }
